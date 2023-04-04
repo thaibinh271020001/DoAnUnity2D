80598c886721a86c51f1db83ab55f5c6b861a57a
Đặng Thái Bình <dangthaibinh10a4@gmail.com>
2023-04-05 02:18:37 +0700

    commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float Jumpspeed = 8f;
+    public float direction = 0f;
+    public Rigidbody2D player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = Input.GetAxis("Horizontal");
+
+        player.velocity = new Vector2(direction * speed, player.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            player.velocity = new Vector2(player.velocity.x, Jumpspeed);
+        }
+    }
+}
